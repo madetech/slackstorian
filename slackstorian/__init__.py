fa67a8f38c22bdd974fca84e170a25eb259739a3
cob16 <public+github@cormacbrady.info>
2019-05-22 16:02:53 +0100

    pull out cli to its own file and pass env config down

---
+++ slackstorian/__init__.py
@@ -0,0 +1,57 @@
+import os
+import sys
+
+import boto3
+from environs import Env
+
+from slackstorian.backup_slack import run_backup
+from slackstorian.slack_client import SlackClient
+import argparse
+
+__version__ = '1.0.0'
+
+
+def main(*foo):
+    args = parse_args(prog=os.path.basename(sys.argv[0]), version=__version__)
+
+    slack = SlackClient(token=get_env('slack_token'))
+
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id=get_env('aws_access_key_id'),
+        aws_secret_access_key=get_env('aws_secret_access_key')
+    )
+
+    run_backup(slack, s3_client, get_env('bucket_name'))
+
+    slack.post_to_channel(
+        channel=get_env('notification_channel'),
+        message='All public channels have been backed up to %s' % get_env('bucket_name')
+    )
+
+
+def parse_args(prog, version):
+    """Parse command-line arguments."""
+    parser = argparse.ArgumentParser(
+        description='A tool for downloading message history from Slack.  This '
+                    'tool downloads the message history for all your public '
+                    'channels, private channels, and direct message threads.',
+        epilog='If your team is on the free Slack plan, this tool can '
+               'download your last 10,000 messages.  If your team is on a paid '
+               'plan, this can download your entire account history.',
+        prog=prog)
+
+    parser.add_argument(
+        '--version', action='version', version='%(prog)s ' + version)
+
+    return parser.parse_args()
+
+
+def get_env(key):
+    enviroment = Env()
+    enviroment.read_env()
+    return enviroment(key)
+
+
+if __name__ == '__main__':
+    main()
--- slackstorian/backup_slack.py
+++ slackstorian/backup_slack.py
@@ -1,17 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8
 
-import argparse
-import json
-import os
-import sys
-import boto3
-from environs import Env
 from tqdm import tqdm
-
-from slackstorian.slack_client import SlackClient
-
-__version__ = '1.0.0'
 
 _USERNAMES_FILE_NAME = 'users.json'
 _PUBLIC_CHANNELS_FILE_NAME = 'channels.json'
@@ -33,65 +23,22 @@ def download_and_save_channels(slack, channels_list):
         save_channel(channel_info=channel, history_json_str=history, path=channel['name'])
 
 
-def parse_args(prog, version):
-    """Parse command-line arguments."""
-    parser = argparse.ArgumentParser(
-        description='A tool for downloading message history from Slack.  This '
-                    'tool downloads the message history for all your public '
-                    'channels, private channels, and direct message threads.',
-        epilog='If your team is on the free Slack plan, this tool can '
-               'download your last 10,000 messages.  If your team is on a paid '
-               'plan, this can download your entire account history.',
-        prog=prog)
-
-    parser.add_argument(
-        '--version', action='version', version='%(prog)s ' + version)
-
-    return parser.parse_args()
-
-
-def save_to_s3(file_body, filename):
-    s3 = boto3.client(
-        's3',
-        aws_access_key_id=get_env('aws_access_key_id'),
-        aws_secret_access_key=get_env('aws_secret_access_key')
-    )
-
+def save_to_s3(s3_client, bucket_name, file_body, filename):
     tqdm.write(f' uploading {filename} to s3')
-    s3.put_object(
+    s3_client.put_object(
         Body=file_body,
-        Bucket=get_env('bucket_name'),
+        Bucket=bucket_name,
         Key=filename
     )
     tqdm.write(f'  done')
 
 
-def get_env(key):
-    enviroment = Env()
-    enviroment.read_env()
-    return enviroment(key)
-
-
-def run_backup(slack):
+def run_backup(slack, s3_client, s3_bucket_name):
     tqdm.write(f'Saving username list to {_USERNAMES_FILE_NAME}')
-    save_to_s3(slack.user_data_json(), _USERNAMES_FILE_NAME)
+    save_to_s3(s3_client, s3_bucket_name, slack.user_data_json(), _USERNAMES_FILE_NAME)
+
     tqdm.write('Saving public channels to %s' % _PUBLIC_CHANNELS_FILE_NAME)
     channels = slack.channels()
-    save_to_s3(channels.as_json(), _PUBLIC_CHANNELS_FILE_NAME)
+    save_to_s3(s3_client, s3_bucket_name, channels.as_json(), _PUBLIC_CHANNELS_FILE_NAME)
+
     download_and_save_channels(slack=slack, channels_list=channels.list)
-
-
-def main(*foo):
-    args = parse_args(prog=os.path.basename(sys.argv[0]), version=__version__)
-
-    slack = SlackClient(token=get_env('slack_token'))
-    run_backup(slack)
-
-    slack.post_to_channel(
-        channel=get_env('notification_channel'),
-        message='All public channels have been backed up to %s' % get_env('bucket_name')
-    )
-
-
-if __name__ == '__main__':
-    main()
