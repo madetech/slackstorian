89016c5a2ef09c78dfb7c812fd6b54b5d1bfa295
Elena <elena@madetech.com>
2019-05-23 17:36:02 +0100

    fetching more than 1000 messages

--- slackstorian/slack_client.py
+++ slackstorian/slack_client.py
@@ -35,12 +35,30 @@ class SlackClient(object):
 
     def _get_history(self, channel_class, channel_id):
         """Returns the message history for a channel"""
+        messages_array = []
+
         last_timestamp = None
+
         response = channel_class.history(channel=channel_id,
                                          latest=last_timestamp,
                                          oldest=0,
                                          count=1000)
-        return response.body['messages']
+
+        messages_array = response.body['messages']
+
+
+        while len(response.body['messages']) == 1000:
+            # import pdb; pdb.set_trace()
+
+            oldest_timestamp = messages_array.pop()['ts']
+
+            response = channel_class.history(channel=channel_id,
+                                             oldest=oldest_timestamp,
+                                             count=1000)
+            messages_array = response.body['messages']
+
+        return messages_array
+
 
     def user_data_json(self):
         """Gets all user data as json"""
@@ -59,4 +77,3 @@ class SlackClient(object):
         return self.slack.chat.post_message(
             channel=channel, text=message, username="Slackstorian"
         )
-
